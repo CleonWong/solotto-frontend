69566a6c48de1ebdd96d6d1b7fe942a5cb74c69f
dhy1996 <dhy1996@live.com.sg>
2021-05-28 02:30:08 -0700

    sollet + anchor integration test

--- src/buyButton.js
+++ src/buyButton.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import solottoProgram from "./interface/solotto";
 
 // ----------
 
 class BuyButton extends Component {
   render() {
-    return <button className="button__active">Buy lottery ticket!</button>;
+    return <button className="button__active" onClick={this.buyTicket}>Buy lottery ticket!</button>;
+  }
+
+  async buyTicket() {
+    let solotto = await solottoProgram();
+    // note: you will need to have initialized the state account with solotto.state.rpc.new() separately
+    // else this will return "no such account"
+    console.log(await solotto.state());
   }
 }
 
---
+++ src/interface/solotto.js
@@ -0,0 +1,31 @@
+import * as anchor from "@project-serum/anchor";
+import { Connection } from "@solana/web3.js";
+import Wallet from "@project-serum/sol-wallet-adapter";
+
+import idl from "./solotto.json";
+
+const CLUSTER = "http://localhost:8899";
+const PROVIDER_URL = "https://www.sollet.io";
+const PROGRAM_ID = "Dsya8jEcxMCewR4emeUB6L1wD8rHWx76p6eAEdfE5Vpw"
+
+async function connectWallet() {
+    let wallet = new Wallet(PROVIDER_URL);
+    wallet.on('connect', publicKey => console.log('Connected to ' + publicKey.toBase58()));
+    wallet.on('disconnect', () => console.log('Disconnected'));
+    await wallet.connect()
+    return wallet
+}
+
+async function solottoProgram() {
+    const wallet = await connectWallet();
+    const provider = new anchor.Provider(
+        new Connection(CLUSTER),
+        wallet,
+        anchor.Provider.defaultOptions()
+    );
+    anchor.setProvider(provider);
+    const programId = new anchor.web3.PublicKey(PROGRAM_ID);
+    return new anchor.Program(idl, programId);
+}
+
+export default solottoProgram;
